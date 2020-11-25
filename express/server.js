5cc15bf3c0d0fc423753038e43864d5ace9e4863
Jayanth <jayanthp17@gmail.com>
2020-11-25 22:19:01 +0530

    fixed route bug

--- express/server.js
+++ express/server.js
@@ -15,8 +15,9 @@ router.get("/", (req, res) => {
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 router.use("/backend", BackRouter);
+router.use("/message", SocketRoute);
 app.use(bodyParser.json());
-app.use("/message", SocketRoute);
+
 app.use("/.netlify/functions/server", router); // path must route to lambda
 app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
